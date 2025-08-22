db8169add689382925803bba5080722172f03b9b
stlawton <lawtons347@gmail.com>
2025-08-22 13:44:03 -0400

    Corrected typos

--- src/sudoku_add_to_copy.py
+++ src/sudoku_add_to_copy.py
@@ -1,6 +1,6 @@
 def copy_and_add(sudolu: list, row_no: int, column_no: int, number: int):
-  new_suoku = []
+  new_sudoku = []
   for item in sudoku:
-    new_sudoku.appened(item)
+    new_sudoku.append(item)
   new_sudoku[row_no][column_no] = number
   return new_sudoku
